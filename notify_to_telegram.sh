5cbeb800a590ade9b3d686d1d899bee0397e1d62
Belloni Alessandro <spupuz@gmail.com>
2026-09-18 11:41:55 +0200

    v1.8.0: merge PRs #77-78, clickable hashtags + grep arg-injection fix
    - Merge PR #77: Add clickable hashtags for easy Telegram filtering
    - Merge PR #78: Fix argument injection vulnerability in grep (--)

--- notify_to_telegram.sh
+++ notify_to_telegram.sh
@@ -47,7 +47,7 @@ SCRIPT_DIR="$(cd "$SCRIPT_DIR" && pwd)"
 SCRIPT_FILE="${BASH_SOURCE[0]##*/}"
 SCRIPT_PATH="$SCRIPT_DIR/$SCRIPT_FILE"
 
-SCRIPT_VERSION="1.7.0"
+SCRIPT_VERSION="1.8.0"
 GITHUB_OWNER="spupuz"
 GITHUB_REPO="duplicati-telegram-notifications"
 GITHUB_RAW_BASE="https://raw.githubusercontent.com/$GITHUB_OWNER/$GITHUB_REPO/main"
